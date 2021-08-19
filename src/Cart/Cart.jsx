2da41fb8f3099314e3ea8d0294fcd5d781ed9576
facundo.osores <facundo.osores@healthintegrator.eu>
2021-08-19 12:28:09 +0200

    create functions to add and remove items from cart

--- src/Cart/Cart.jsx
+++ src/Cart/Cart.jsx
@@ -13,6 +13,8 @@ type Props = {
 };
 
 const Cart: React.FC<Props> = ({ cartItems, addToCart, removeFromCart }) => {
+  const calculateTotal = (items: CartItemType[]) =>
+    items.reduce((ack: Number, item) => ack + item.amount * item.price, 0);
   return (
     <Wrapper>
       <h2>Your Shopping Cart</h2>
@@ -20,6 +22,7 @@ const Cart: React.FC<Props> = ({ cartItems, addToCart, removeFromCart }) => {
       {cartItems.map((item) => (
         <CartItem key={item.id} item={item} addToCart={addToCart} removeFromCart={removeFromCart} />
       ))}
+      <h2>Total: ${calculateTotal(cartItems).toFixed(2)}</h2>
     </Wrapper>
   );
 };
--- src/CartItem/CartItem.jsx
+++ src/CartItem/CartItem.jsx
@@ -19,14 +19,14 @@ const CartItem: React.FC = ({ item, addToCart, removeFromCart }) => (
 
       <div className="information">
         <p>Price: ${item.price}</p>
-        <p>Total: ${item.amount * item.price}.toFixed(2</p>
+        <p>Total: ${(item.amount * item.price).toFixed(2)}</p>
       </div>
       <div className="buttons">
         <Button size="small" disableElevation variant="contained" onClick={() => removeFromCart(item.id)}>
           -
         </Button>
         <p>{item.amount}</p>
-        <Button size="small" disableElevation variant="contained" onClick={() => removeFromCart(item)}>
+        <Button size="small" disableElevation variant="contained" onClick={() => addToCart(item)}>
           +
         </Button>
       </div>
